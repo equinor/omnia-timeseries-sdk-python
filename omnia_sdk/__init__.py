bce422577cf3644a08528318161427cf5ab40bb7
Per Erlend Voie <per.voie@gmail.com>
2019-11-13 17:50:40 +0100

    Add __version__ and __summary__ to init.

--- omnia_sdk/__init__.py
+++ omnia_sdk/__init__.py
@@ -1,4 +1,16 @@
 """
 Omnia Python SDK
 """
+from pkg_resources import get_distribution, DistributionNotFound
 from .client import OmniaClient
+
+
+__summary__ = __doc__
+
+# get version
+try:
+    # version at runtime from distribution/package info
+    __version__ = get_distribution("omnia_timeseries_sdk").version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = ""
