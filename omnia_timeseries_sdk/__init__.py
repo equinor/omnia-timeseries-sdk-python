992bd40928b59a372934aeac69295fd9a1bdd917
tovop <per.voie@gmail.com>
2019-11-13 20:45:24 +0100

    Cleanup.

--- omnia_timeseries_sdk/__init__.py
+++ omnia_timeseries_sdk/__init__.py
@@ -5,9 +5,6 @@ from pkg_resources import get_distribution, DistributionNotFound
 from .client import OmniaClient
 
 
-__summary__ = __doc__
-
-# get version
 try:
     # version at runtime from distribution/package info
     __version__ = get_distribution("omnia_timeseries_sdk").version
